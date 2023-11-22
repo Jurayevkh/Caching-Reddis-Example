1d54b60349dea700e381990f70eb7543e8d7b9e1
Jurayevkh <jurayevkh@gmail.com>
2023-11-22 10:23:32 +0500

    calls Fluent API

---
+++ Caching-Example/DataAccess/UserDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Caching_Example.Configuration;
+using Caching_Example.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Caching_Example.DataAccess
+{
+	public class UserDBContext:DbContext
+	{
+		public UserDBContext(DbContextOptions<UserDBContext> options):base(options)
+		{
+
+		}
+
+		public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+			modelBuilder.ApplyConfiguration(new UserTypeConfiguration());
+		}
+    }
+}
+
