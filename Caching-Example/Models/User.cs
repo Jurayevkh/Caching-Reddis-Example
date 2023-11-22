f3cf1fac8cb029e7aa0fe0baf9927c3d38da956c
Jurayevkh <jurayevkh@gmail.com>
2023-11-22 10:24:38 +0500

    Created User Model

---
+++ Caching-Example/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Caching_Example.Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string UserName { get; set; }
+		public int Age { get; set; }
+	}
+}
+
